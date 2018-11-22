c7f310793c1e9500edbff05f8c30872527bc4d7f
UDAY BAG <uday625@Nemesis-2.local>
2018-11-22 09:57:28 -0500

    added prop types

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
 import './Counter.css';
 
 
@@ -20,5 +21,11 @@ const Counter = (props) => {
         </Fragment>
     );
 }
+
+
+Counter.propTypes = {
+    ticker: PropTypes.number.isRequired
+}
+
  
 export default Counter;
